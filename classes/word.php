dda9400bbb14422805073ba9147e8e21e3160eb7
danik <d.shumakovich@gmail.com>
2020-11-20 00:47:25 +0300

    Update class word.

--- classes/word.php
+++ classes/word.php
@@ -9,14 +9,14 @@ class word
     /**
      * @var string Main word.
      */
-    private $word;
+    private string $word;
 
     /**
      * @var string A few words to the left and right of the main word.
      */
-    private $context;
+    private string $context;
 
-    public function __construct($word, $context)
+    public function __construct(string $word, string $context)
     {
         $this->word = $word;
         $this->context = $context;
@@ -41,6 +41,6 @@ class word
      */
     public function toCheckbox()
     {
-        return "<input type='checkbox' name='{$this->word}'>{$this->word}   {$this->context}<br>";
+        return "<input type='checkbox' name='{$this->word}'>{$this->word}";
     }
 }
