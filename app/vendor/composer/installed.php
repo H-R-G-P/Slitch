a11552c3bad5ed157e80d66ad0fd101d1477715a
danik <d.shumakovich@gmail.com>
2021-03-11 15:26:48 +0100

    Add folder "Vendor"

---
+++ app/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'c411ad9663c91d9746128a89a5892e2850120652',
+    'name' => 'vendor_name/libraries',
+  ),
+  'versions' => 
+  array (
+    'erusev/parsedown' => 
+    array (
+      'pretty_version' => '1.7.4',
+      'version' => '1.7.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'cb17b6477dfff935958ba01325f2e8a2bfa6dab3',
+    ),
+    'vendor_name/libraries' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c411ad9663c91d9746128a89a5892e2850120652',
+    ),
+  ),
+);
