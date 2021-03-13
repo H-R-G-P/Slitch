333685ccf5254da715e549a270a2dd71e393b251
danik <d.shumakovich@gmail.com>
2021-03-13 19:30:02 +0100

    Add interface for sorting words on columns

--- app/views/stuffs/handle.php
+++ app/views/stuffs/handle.php
@@ -25,6 +25,9 @@
     </div>
     <ul class="navbar-nav ml-auto">
       <li class="nav-item">
+        <a class="nav-link" href="#" data-toggle="modal" data-target="#sortSettingsModal">Sort words</a>
+      </li>
+      <li class="nav-item">
         <a class="nav-link" onclick="putInModalWords()" href="#" data-toggle="modal" data-target="#wordsModal">Words</a>
       </li>
       <li class="nav-item">
@@ -46,6 +49,37 @@
       <div class="modal-body" id="listOfWordsInModal"></div>
       <div class="modal-footer">
         <button type="button" onclick="copyToClipboard('listOfWordsInModal')" class="btn btn-primary mr-auto">Copy</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="sortSettingsModal" tabindex="-1" role="dialog" aria-labelledby="settingsTitle" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title ml-auto" id="settingsTitle">Settings</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+          <label for="columnsCount">Column count</label>
+          <input id="columnsCount" class="form-control" type="number" value="3">
+          <label for="rowsCount" class="mt-3">Words count in column</label>
+          <input id="rowsCount" class="form-control" type="number" value="94">
+          <label for="words" class="mt-3">Words separated by enters</label>
+          <textarea id="words" class="form-control"></textarea>
+          <div class="mt-3 row" id="columnsButtons">
+
+          </div>
+          <div id="columnsWords" class="row">
+
+          </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" onclick="sort()" class="btn btn-primary">Sort</button>
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
       </div>
     </div>
@@ -171,6 +205,44 @@
         for (let i = 0; i < wordsInContext.length; ++i) {
             modalBody.innerHTML += (wordsInContext[i]+'<br>');
         }
+    }
+
+    function sort() {
+        let outputButtons = document.getElementById('columnsButtons');
+        let outputWords = document.getElementById('columnsWords');
+        let text = document.getElementById('words').value;
+        let columnsCount = document.getElementById('columnsCount').value;
+        let rowsCount = document.getElementById('rowsCount').value;
+        let words = text.split('\n');
+
+        let table = [];
+        for (let i = 0; i < columnsCount; i++) {
+            table[i] = '';
+        }
+        for (let i = 0; i < words.length; i += rowsCount * columnsCount) {
+            for (let g = 0; g < columnsCount; g++) {
+                for (let j = 0; j < rowsCount; ++j)
+                {
+                    let key = j + i + (g * rowsCount)
+                    if (typeof words[key] !== 'undefined') {
+                        table[g] += (words[key] + '<br>');
+                    }
+                }
+            }
+        }
+        for (let i = 0; i < columnsCount; i++) {
+            table[i] = table[i].substring(0, table[i].length - 4);
+        }
+        let outputButtonsHTML = '';
+        let outputWordsHTML = '';
+        for (let i = 0; i < columnsCount; i++) {
+            let div = '<div class="col-3"><b>' + i + ':</b><p id="column' + i + '">' + table[i] + '</p></div>';
+            let btn = '<div class="col-3"><b>' + i + ': </b><button type="button" onclick="copyToClipboard(' + "'column" + i + "'" + ')" class="btn btn-primary my-3">Copy</button></div>';
+            outputButtonsHTML += btn;
+            outputWordsHTML += div;
+        }
+        outputButtons.innerHTML = outputButtonsHTML;
+        outputWords.innerHTML = outputWordsHTML;
     }
 </script>
 <?php require APPROOT.'/views/inc/footer.php'; ?>
