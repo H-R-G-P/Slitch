466e78bc7fe90beb1a643384056eb0e8a76f6cc4
danik <d.shumakovich@gmail.com>
2021-03-16 11:13:10 +0100

    Add test for TextProcessor::processDots

--- app/tests/TextProcessorTest.php
+++ app/tests/TextProcessorTest.php
@@ -48,4 +48,49 @@ class TextProcessorTest extends TestCase
             "Not correctly process some hyphens in a word."
         );
     }
+
+    public function testProcessDots()
+    {
+        $processor = new TextProcessor();
+
+        $text = ".......Start";
+        $expectedText = "Start";
+        self::assertSame(
+            str_split($expectedText),
+            $processor->processDots(str_split($text)),
+            "Not correctly process some dots in the row in the begin of text."
+        );
+
+        $text = "Sta.....rt";
+        $expectedText = "Start";
+        self::assertSame(
+            str_split($expectedText),
+            $processor->processDots(str_split($text)),
+            "Not correctly process some dots in the row in a word."
+        );
+
+        $text = "Start ......End";
+        $expectedText = "Start End";
+        self::assertSame(
+            str_split($expectedText),
+            $processor->processDots(str_split($text)),
+            "Not correctly process some dots in the row when 2'nd of 2 sentences begins with it."
+        );
+
+        $text = "Start......";
+        $expectedText = "Start.";
+        self::assertSame(
+            str_split($expectedText),
+            $processor->processDots(str_split($text)),
+            "Not correctly process many dots in the row in the end of text."
+        );
+
+        $text = "Start...... End";
+        $expectedText = "Start. End";
+        self::assertSame(
+            str_split($expectedText),
+            $processor->processDots(str_split($text)),
+            "Not correctly process many dots in the row when 1'st of 2 sentences ends with it."
+        );
+    }
 }
