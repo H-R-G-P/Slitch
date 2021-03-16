00623802b79f324945bbbd0b397255588b9ab4d6
danik <d.shumakovich@gmail.com>
2021-03-16 11:10:45 +0100

    Add test for TextProcessor::processHyphens

--- app/tests/TextProcessorTest.php
+++ app/tests/TextProcessorTest.php
@@ -4,5 +4,48 @@ use PHPUnit\Framework\TestCase;
 
 class TextProcessorTest extends TestCase
 {
+    public function testProcessHyphens()
+    {
+        $processor = new TextProcessor();
 
+        $text = "-----Start";
+        $expectedText = "Start";
+        self::assertSame(
+            str_split($expectedText),
+            $processor->processHyphens(str_split($text)),
+            "Not correctly process some hyphens in the begin of text."
+        );
+
+        $text = "End-----";
+        $expectedText = "End";
+        self::assertSame(
+            str_split($expectedText),
+            $processor->processHyphens(str_split($text)),
+            "Not correctly process some hyphens in the end of text."
+        );
+
+        $text = "Start----- End";
+        $expectedText = "Start End";
+        self::assertSame(
+            str_split($expectedText),
+            $processor->processHyphens(str_split($text)),
+            "Not correctly process some hyphens when 1'st of 2 sentences ends with it."
+        );
+
+        $text = "Start -----End";
+        $expectedText = "Start End";
+        self::assertSame(
+            str_split($expectedText),
+            $processor->processHyphens(str_split($text)),
+            "Not correctly process some hyphens when 2'nd of 2 sentences begins with it."
+        );
+
+        $text = "Start-----End";
+        $expectedText = "Start-End";
+        self::assertSame(
+            str_split($expectedText),
+            $processor->processHyphens(str_split($text)),
+            "Not correctly process some hyphens in a word."
+        );
+    }
 }
