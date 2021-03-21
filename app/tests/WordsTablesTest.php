afa01cd0fcc1daf442a17fb6b9ff85c0af819b6f
danik <d.shumakovich@gmail.com>
2021-03-21 18:33:28 +0100

    Add sub-test in testArray_diff_inLowercase

--- app/tests/WordsTablesTest.php
+++ app/tests/WordsTablesTest.php
@@ -24,5 +24,21 @@ class WordsTablesTest extends TestCase
             'STAY2',
         );
         self::assertSame($expected, $actual);
+
+        $array1 = array(
+            new Word('sTay1', ''),
+            new Word('Leave1', ''),
+            new Word('STAY2', ''),
+            new Word('LEAVE2', ''),
+        );
+        $array2 = array(
+            'leave1',
+        );
+        $array3 = array(
+            'leave2',
+        );
+        $words = WordsTables::array_diff_inLowercase($array1, $array2, $array3);
+        self::assertSame('sTay1', "$words[0]");
+        self::assertSame('STAY2', "$words[1]");
     }
 }
