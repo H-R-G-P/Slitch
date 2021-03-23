22e007145fb83862f09c55e1b1bff885122e78ed
danik <d.shumakovich@gmail.com>
2021-03-23 18:28:05 +0100

    Add test for method WordsTables::getNotLearnedWordsFrom

--- app/tests/WordsTablesTest.php
+++ app/tests/WordsTablesTest.php
@@ -4,6 +4,17 @@ use \PHPUnit\Framework\TestCase;
 
 class WordsTablesTest extends TestCase
 {
+    public function testGetNotLearnedWordsFrom()
+    {
+        define('DB_HOST', 'localhost');
+        define('DB_USER', 'slitch');
+        define('DB_PASS', 'slitch-psw');
+        define('DB_NAME', 'slitch');
+        $word = new WordsTables();
+
+        self::assertSame(['sds'], $word->getNotLearnedWordsFrom(['sds', 'on'], 'english'));
+    }
+
     public function testArray_diff_inLowercase()
     {
         $array1 = array(
