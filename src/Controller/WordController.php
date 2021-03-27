c451c14a1b4f8d37b4cea9b38ce0ab51e277c46c
danik <d.shumakovich@gmail.com>
2021-03-27 19:39:34 +0100

    Add frame for WordController

---
+++ src/Controller/WordController.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class WordController
+ * @Route("/word")
+ * @package App\Controller
+ */
+class WordController extends AbstractController
+{
+    /**
+     * @Route("/add", name="add_words", methods{"POST"})
+     */
+    public function add() : Response
+    {
+        return new Response();
+    }
+
+    /**
+     * @Route("/add", name="add_words_form", methods{"GET"})
+     */
+    public function formAdd() : Response
+    {
+        return new Response();
+    }
+
+    /**
+     * @Route("/delete", name="delete_words", methods{"POST"})
+     */
+    public function delete() : Response
+    {
+        return new Response();
+    }
+
+    /**
+     * @Route("/delete", name="delete_words_form", methods{"GET"})
+     */
+    public function formDelete() : Response
+    {
+        return new Response();
+    }
+}
