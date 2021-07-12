2c98f24f57648f22cb8299dff95753a3ebabb366
danik <d.shumakovich@gmail.com>
2021-07-12 14:18:48 +0200

    Add remaining entities with repositories

---
+++ src/Entity/PolishUntranslatablewords.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * PolishUntranslatablewords
+ *
+ * @ORM\Table(name="polish_untranslatableWords", uniqueConstraints={@ORM\UniqueConstraint(name="polish_notLearnedWords_word_uindex", columns={"word"})})
+ * @ORM\Entity(repositoryClass="App\Repository\PolishUntranslatablewordsRepository")
+ */
+class PolishUntranslatablewords
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="word", type="string", length=25, nullable=false)
+     */
+    private $word;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getWord(): ?string
+    {
+        return $this->word;
+    }
+
+    public function setWord(string $word): self
+    {
+        $this->word = $word;
+
+        return $this;
+    }
+
+
+}
