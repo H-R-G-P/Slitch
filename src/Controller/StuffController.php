2a5994adb726f43b5c90138a800fc0ca72650690
danik <d.shumakovich@gmail.com>
2021-07-15 10:06:49 +0200

    Add functions in controller Stuff: handlePageEn(), handleEn(), handlePagePl()

--- src/Controller/StuffController.php
+++ src/Controller/StuffController.php
@@ -3,10 +3,19 @@
 
 namespace App\Controller;
 
+use App\Entity\EnglishUntranslatableWords;
+use App\Entity\EnglishWords;
 use App\Entity\Languages;
+use App\Entity\PolishUntranslatableWords;
+use App\Entity\PolishWords;
 use App\Entity\Stuff;
 use App\Form\StuffType;
+use App\Repository\EnglishUntranslatableWordsRepository;
+use App\Repository\EnglishWordsRepository;
+use App\Repository\PolishUntranslatableWordsRepository;
+use App\Repository\PolishWordsRepository;
 use App\Repository\StuffRepository;
+use App\Service\Helper;
 use App\Service\TextProcessor;
 use phpDocumentor\Reflection\Types\This;
 use Symfony\Component\HttpFoundation\Request;
@@ -171,13 +180,110 @@ class StuffController extends AbstractController
     }
 
     /**
-     * @Route("/handle/{id}", name="handle_stuff_form", methods={"GET"}, requirements={"id"="%app.id_regex%"})
+     * @Route("/handle/en/{id}", name="handle_english_stuff_form", methods={"GET"}, requirements={"id"="%app.id_regex%"})
      * @param int $id
      * @return Response
      */
-    public function handlePage(int $id) : Response
+    public function handlePageEn(int $id, StuffRepository $stuffRep, EnglishWordsRepository $enWordsRep, EnglishUntranslatableWordsRepository $enUntranslatableWordsRep, TextProcessor $textProcessor, Helper $helper) : Response
     {
+        $stuff = $stuffRep->findOneBy([
+            'id' => $id,
+        ]);
+        if (!$stuff) {
+            $this->addFlash('info', "Stuff with id: $id does not exist");
+            return $this->redirectToRoute('show_all_stuffs');
+        }
 
+        try {
+            $decodeText = html_entity_decode($stuff->getText(), ENT_QUOTES, 'utf-8');
+            $uniqWords = $textProcessor->getUniqWords($decodeText, $stuff->getLanguage());
+            $notLearnedWords = $helper::array_diff_inLowercase($uniqWords, $enWordsRep->findAll(), $enUntranslatableWordsRep->findAll());
+            $uniqWordsObj = $textProcessor->getUniqWordsObj($decodeText, $stuff->getLanguage());
+            $notLearnedWordsObj = array_intersect($uniqWordsObj, $notLearnedWords);
+        }catch (\Exception $e) {
+            $this->addFlash('danger', $e->getMessage());
+            return $this->redirectToRoute('show_all_stuffs');
+        }
+
+        if (count($notLearnedWordsObj) === 0)
+        {
+            $this->addFlash('info', 'All words in this stuff are learned.');
+            $this->redirectToRoute('show_all_stuffs');
+        }
+
+        return $this->render('stuff/handle.html.twig', [
+            'notLearnedWords' => $notLearnedWordsObj,
+            'stuff' => $stuff,
+        ]);
+    }
+
+    /**
+     * @Route("/handle/en/{id}", name="handle_english_stuff", methods={"POST"}, requirements={"id"="%app.id_regex%"})
+     * @param int $id
+     * @return Response
+     */
+    public function handleEn(int $id, Request $request) : Response
+    {
+        $em = $this->getDoctrine()->getManager();
+        if ($request->request->get('learnedWords'))
+        {
+            foreach ($request->request->get('learnedWords') as $learnedWord) {
+                $word = new EnglishWords();
+                $word->setWord($learnedWord);
+                $em->persist($word);
+            }
+            $em->flush();
+        }
+
+        if ($request->request->get('untranslatableWords'))
+        {
+            foreach ($request->request->get('untranslatableWords') as $untranslatableWords) {
+                $word = new EnglishUntranslatableWords();
+                $word->setWord($untranslatableWords);
+                $em->persist($word);
+            }
+            $em->flush();
+        }
+
+        return new Response();
+    }
+
+    /**
+     * @Route("/handle/pl/{id}", name="handle_polish_stuff_form", methods={"GET"}, requirements={"id"="%app.id_regex%"})
+     * @param int $id
+     * @return Response
+     */
+    public function handlePagePl(int $id, StuffRepository $stuffRep, PolishWordsRepository $plWordsRep, PolishUntranslatableWordsRepository $plUntranslatableWordsRep, TextProcessor $textProcessor, Helper $helper) : Response
+    {
+        $stuff = $stuffRep->findOneBy([
+            'id' => $id,
+        ]);
+        if (!$stuff) {
+            $this->addFlash('info', "Stuff with id: $id does not exist");
+            return $this->redirectToRoute('show_all_stuffs');
+        }
+
+        try {
+            $decodeText = html_entity_decode($stuff->getText(), ENT_QUOTES, 'utf-8');
+            $uniqWords = $textProcessor->getUniqWords($decodeText, $stuff->getLanguage());
+            $notLearnedWords = $helper::array_diff_inLowercase($uniqWords, $plWordsRep->findAll(), $plUntranslatableWordsRep->findAll());
+            $uniqWordsObj = $textProcessor->getUniqWordsObj($decodeText, $stuff->getLanguage());
+            $notLearnedWordsObj = array_intersect($uniqWordsObj, $notLearnedWords);
+        }catch (\Exception $e) {
+            $this->addFlash('danger', $e->getMessage());
+            return $this->redirectToRoute('show_all_stuffs');
+        }
+
+        if (count($notLearnedWordsObj) === 0)
+        {
+            $this->addFlash('info', 'All words in this stuff are learned.');
+            $this->redirectToRoute('show_all_stuffs');
+        }
+
+        return $this->render('stuff/handle.html.twig', [
+            'notLearnedWords' => $notLearnedWordsObj,
+            'stuff' => $stuff,
+        ]);
     }
 
     /**
