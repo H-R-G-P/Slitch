23cd829fa56eb7f22852576ab7801ba8ab5cd45c
danik <d.shumakovich@gmail.com>
2021-07-16 18:08:43 +0200

    Create form for DTO "Words"

---
+++ src/Form/WordsType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Dto\Words;
+use App\Entity\Languages;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class WordsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('language', EntityType::class, [
+                'class' => Languages::class,
+                'choice_label' => 'name',
+            ])
+            ->add('learnedWords', TextareaType::class)
+            ->add('untranslatableWords', TextareaType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Words::class,
+        ]);
+    }
+}
