08ca1c3c2a9deae013a8e0b9ea75c84a5a42b51f
danik <d.shumakovich@gmail.com>
2021-07-26 16:51:24 +0200

    Add method to StuffControllerService

--- src/Service/StuffControllerService.php
+++ src/Service/StuffControllerService.php
@@ -4,13 +4,17 @@
 namespace App\Service;
 
 
+use App\Entity\LearnedWords;
 use App\Entity\Stuff;
+use App\Entity\UntranslatableWords;
 use App\Repository\LearnedWordsRepository;
+use App\Repository\StuffRepository;
 use App\Repository\UntranslatableWordsRepository;
 use App\Vo\Word;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
 
-class StuffControllerService extends AbstractController
+class StuffControllerService
 {
     /**
      * @param Stuff $stuff
@@ -33,5 +37,40 @@ class StuffControllerService extends AbstractController
         $uniqWordsObj = $textProcessor->getUniqWordsObj($decodeText, $stuff->getLanguage());
 
         return array_intersect($uniqWordsObj, $notLearnedWords);
+    }
+
+    /**
+     * @param Request $request
+     * @param ObjectManager $em
+     * @param int $stuffId
+     * @param StuffRepository $stuffRepository
+     */
+    public function addWordsToDb(Request $request, ObjectManager $em, int $stuffId, StuffRepository $stuffRepository) : void
+    {
+        $stuff = $stuffRepository->findOneBy([
+            'id' => $stuffId,
+        ]);
+
+        if ($request->request->get('learnedWords'))
+        {
+            foreach ($request->request->get('learnedWords') as $learnedWord) {
+                $word = new LearnedWords();
+                $word->setWord($learnedWord);
+                $word->setIdLanguage($stuff->getLanguage());
+                $em->persist($word);
+            }
+            $em->flush();
+        }
+
+        if ($request->request->get('untranslatableWords'))
+        {
+            foreach ($request->request->get('untranslatableWords') as $untranslatableWords) {
+                $word = new UntranslatableWords();
+                $word->setWord($untranslatableWords);
+                $word->setIdLanguage($stuff->getLanguage());
+                $em->persist($word);
+            }
+            $em->flush();
+        }
     }
 }
