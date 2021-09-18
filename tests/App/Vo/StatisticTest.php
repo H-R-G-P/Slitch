ce187899d1f8e84ef7ccbe988c44b84025faf3a5
danik <d.shumakovich@gmail.com>
2021-09-18 17:25:35 +0200

    Upgrade StatisticTest class.

--- tests/App/Vo/StatisticTest.php
+++ tests/App/Vo/StatisticTest.php
@@ -19,6 +19,62 @@ class StatisticTest extends TestCase
             ['t', 'g', 'l'],
         ));
         self::assertSame([1 => 't'], $actual);
+
+        $actual = $stat->intersect(array(
+            ['i', 't', 't'],
+            ['t', 'g', 'k'],
+            ['t', 'g', 'l'],
+        ));
+        self::assertSame([1 => 't'], $actual);
+    }
+
+    public function testMerge()
+    {
+        $stat = new Statistic();
+
+        $a1 = array(
+            'i' => 1,
+        );
+        $a2 = array(
+            't' => 3,
+            'f' => 2,
+        );
+        $act = $stat->merge($a1, $a2);
+        self::assertSame(array(
+            'i' => 1,
+            't' => 3,
+            'f' => 2,
+        ), $act);
+    }
+
+    public function testGetSum()
+    {
+        $stat = new Statistic();
+
+        $a2 = array(
+            't' => 3,
+            'f' => 2,
+            'g' => 1,
+            'l' => 32,
+        );
+        $a1 = array(
+            't', 'l',
+        );
+        self::assertSame(35, $stat->getSum($a2, $a1));
+    }
+
+    public function testCountRepeats()
+    {
+        $stat = new Statistic();
+
+        $a1 = array(
+            't', 't', 'r', 't',
+        );
+
+        self::assertSame([
+            't' => 3,
+            'r' => 1,
+        ], $stat->countRepeats($a1));
     }
 
     public function testSetMatches()
@@ -28,11 +84,20 @@ class StatisticTest extends TestCase
 
         $texts->setLanguage((new Languages())->setName('english'));
         $texts->setTexts(array(
-            'i t g',
+            'i t t',
             't f k',
-            't g l h q w p',
+            't g l u',
         ));
-        $stat->setMatches($texts);
-        self::assertSame(10, $stat->getMatches());
+        $stat->setMatchesAll($texts);
+        self::assertSame(40, $stat->getMatchesAll());
+
+        $texts->setLanguage((new Languages())->setName('english'));
+        $texts->setTexts(array(
+            'I am danik',
+            'I am Tania',
+            'i Am Roma',
+        ));
+        $stat->setMatchesAll($texts);
+        self::assertSame(66, $stat->getMatchesAll());
     }
 }
