1cdd3e380c001feb8f5fd4750ea015a73bce4d27
danik <d.shumakovich@gmail.com>
2021-09-19 22:14:20 +0200

    Add new properties in statistic.

--- src/Vo/Repetition.php
+++ src/Vo/Repetition.php
@@ -16,14 +16,29 @@ use App\Service\TextProcessor;
 class Repetition
 {
     /**
+     * @var int Percentage of words that repeats two times.
+     */
+    private int $rarePercentage = 0;
+
+    /**
      * @var int Count of words that repeats two times.
      */
     private int $rare = 0;
 
     /**
+     * @var int Percentage of words that repeats 3-5 times.
+     */
+    private int $averagePercentage = 0;
+
+    /**
      * @var int Count of words that repeats 3-5 times.
      */
     private int $average = 0;
+
+    /**
+     * @var int Percentage of words that repeats >5 times.
+     */
+    private int $frequentPercentage = 0;
 
     /**
      * @var int Count of words that repeats >5 times.
@@ -58,6 +73,10 @@ class Repetition
         $this->rare = $rare;
         $this->average = $average;
         $this->frequent = $frequent;
+
+        $this->rarePercentage = ($rare * 100)/count($allRepeats);
+        $this->averagePercentage = ($average * 100)/count($allRepeats);
+        $this->frequentPercentage = ($frequent * 100)/count($allRepeats);
     }
 
     /**
@@ -82,5 +101,29 @@ class Repetition
     public function getFrequent(): int
     {
         return $this->frequent;
+    }
+
+    /**
+     * @return int
+     */
+    public function getRarePercentage(): int
+    {
+        return $this->rarePercentage;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAveragePercentage(): int
+    {
+        return $this->averagePercentage;
+    }
+
+    /**
+     * @return int
+     */
+    public function getFrequentPercentage(): int
+    {
+        return $this->frequentPercentage;
     }
 }
